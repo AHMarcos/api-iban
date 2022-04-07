bb8eb72056581a1479484ec92acdfff219cd4659
marcos <marcos.alarcon@global66.com>
2022-04-07 12:25:20 -0500

    xd

---
+++ src/main/java/com/marcosalarcon/apiiban/ApiIbanApplication.java
@@ -0,0 +1,13 @@
+package com.marcosalarcon.apiiban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiIbanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiIbanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marcosalarcon/apiiban/ApiIbanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosalarcon.apiiban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiIbanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
